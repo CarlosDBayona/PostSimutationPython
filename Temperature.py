aa4c162554c764b30b27f298393681b9c9caea51
Carlos <carlosdanielbayo@gmail.com>
2018-10-19 01:55:57 -0500

    Working as expected@

---
+++ Temperature.py
@@ -0,0 +1,12 @@
+import math
+from time import gmtime, strftime
+import time
+import requests
+import random
+import json
+while True:
+    data = random.randint(0, 10)
+    showtime = strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    x = {"data": data, "date": showtime}
+    requests.post('http://localhost:3000/api/Temperature',x)
+    time.sleep(1)
